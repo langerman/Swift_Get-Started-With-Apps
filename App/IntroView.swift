1e24fcd5683e1b2845f55b5bd848ab7e9e59b377
Daniel Langerman <08_tears.meter@icloud.com>
2023-07-09 11:21:46 -0400

    Create IntroView.swift
    Inside the file, you'll see the name of the view you're looking at. Here, you're looking at the view named IntroView.
    
    Inside IntroView, you'll see a property named body. Inside the body is where you'll start writing SwiftUI code.
    
    Inside the body, you'll see a Text view, which displays the text "Hello, friend." in the preview.
    
    Speaking of the preview, this is the code that generates the IntroView preview. You'll see similar code under many views to help you visualize the code you write.

---
+++ App/IntroView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct IntroView: View {
+   var body: some View {
+       Text("Hellow, friend.")
+   }
+}
+
+struct IntroView_Previews: PreviewProvider {
+   static var previews: some View {
+      VStack {
+         IntroView()
+      }
+   }
+}
